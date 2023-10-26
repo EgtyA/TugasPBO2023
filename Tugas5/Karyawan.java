e6ccd0d47e2400615263c0e36ec0d1015b1813fa
EgtyA <149040227+EgtyA@users.noreply.github.com>
2023-10-26 13:12:56 +0700

    Add files via upload

---
+++ Tugas5/Karyawan.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TampilanGuiDbPertemuan5;
+
+/**
+ *
+ * @author egty
+ */
+public class Karyawan {
+    
+    private String idKaryawan;
+    private String idDep;
+    private String nama;
+    private String alamat;
+
+    /**
+     * @return the idKaryawan
+     */
+    public String getIdKaryawan() {
+        return idKaryawan;
+    }
+
+    /**
+     * @param idKaryawan the idKaryawan to set
+     */
+    public void setIdKaryawan(String idKaryawan) {
+        this.idKaryawan = idKaryawan;
+    }
+
+    /**
+     * @return the idDep
+     */
+    public String getIdDep() {
+        return idDep;
+    }
+
+    /**
+     * @param idDep the idDep to set
+     */
+    public void setIdDep(String idDep) {
+        this.idDep = idDep;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+}
