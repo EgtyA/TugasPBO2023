889f5ce5e878147edcdcd33b4434c29939c7f5a3
EgtyA <149040227+EgtyA@users.noreply.github.com>
2023-11-05 18:53:16 +0700

    Update UtsBuku.java

--- Tugas9/UtsBuku.java
+++ Tugas9/UtsBuku.java
@@ -59,31 +59,6 @@ public class UtsBuku extends javax.swing.JFrame {
 
     ArrayList<Buku> dataBuku;
 
-    private int masukkanData(Connection conn, String Isbn, String judul_buku, String tahun_terbit, String penerbit) throws SQLException {
-        PreparedStatement pst = conn.prepareStatement("INSERT INTO buku (isbn, judul_buku, tahun_terbit, penerbit) VALUES(?,?,?,?)");
-        pst.setString(1, Isbn);
-        pst.setString(2, judul_buku);
-        pst.setString(3, tahun_terbit);
-        pst.setString(4, penerbit);
-        return pst.executeUpdate();
-    }
-
-    private int updateData(Connection conn, String Isbn, String judul_buku, String tahun_terbit, String penerbit) throws SQLException {
-        PreparedStatement pst = conn.prepareStatement("UPDATE buku set judul_buku = ?, tahun_terbit = ?, penerbit = ? WHERE isbnGu = ?");
-
-        pst.setString(1, judul_buku);
-        pst.setString(2, tahun_terbit);
-        pst.setString(3, penerbit);
-        pst.setString(4, Isbn);
-        return pst.executeUpdate();
-    }
-
-    private int hapusData(Connection conn, String judul_buku) throws SQLException {
-        PreparedStatement pst = conn.prepareStatement("DELETE FROM buku WHERE judul_buku = ?");
-        pst.setString(1, judul_buku);
-        return pst.executeUpdate();
-    }
-
     private void tampil(Connection conn) {
         dataBuku.clear();
         try {
@@ -407,10 +382,6 @@ public class UtsBuku extends javax.swing.JFrame {
         jTextFieldisbn.setText("");
         kosongkan_form();
     }//GEN-LAST:event_jButtonhapusActionPerformed
-
-    private void jScrollPane1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jScrollPane1MouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jScrollPane1MouseClicked
 
     private void jButtoncetakActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtoncetakActionPerformed
         // TODO add your handling code here:
